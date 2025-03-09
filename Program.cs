9bc712a75f87ad5952033eae757a71654ff8ce5b
יהודית מלול <yuma8153@gmail.com>
2025-03-09 03:15:01 +0200

    Update Environment variable

--- Program.cs
+++ Program.cs
@@ -4,8 +4,13 @@ using TodoApi;
 var builder = WebApplication.CreateBuilder(args);
 // הזרקת ה-DbContext לשירותים
 builder.Services.AddDbContext<ToDoDbContext>(options =>
-    options.UseMySql(builder.Configuration.GetConnectionString("ToDoDB"), 
-    Microsoft.EntityFrameworkCore.ServerVersion.Parse(" 8.0.22-mysql")));
+    // options.UseMySql(builder.Configuration.GetConnectionString("ToDoDB"), 
+    // Microsoft.EntityFrameworkCore.ServerVersion.Parse(" 8.0.22-mysql")));
+    options.UseMySql(
+        Environment.GetEnvironmentVariable("ToDoDB"), // קריאה למשתנה הסביבה
+        Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.22-mysql"))); // הגדרת הגרסה של MySQL
+
+  
 
 // הוספת שירות CORS
 builder.Services.AddCors(options =>
